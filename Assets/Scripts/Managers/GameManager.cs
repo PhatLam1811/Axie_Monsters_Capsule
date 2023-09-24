6ee3e3041a9c07f086ff9424aad40c49d0e1b5a0
phatlam1811 <phatlam1811@gmail.com>
2023-09-25 03:25:17 +0700

    Add GameDataManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    [SerializeField] private Transform managersContainer;
+
     void Start()
     {
-
+        this.OpenGame();
     }
 
     public void OpenGame()
     {
+        DontDestroyOnLoad(this.managersContainer);
 
+        GameDataManager.Instance.OpenGame();
     }
 }
